a192552c169fc250c73af683cac54c690e51c083
CtrlKismet <2464676281@qq.com>
2018-08-03 23:49:54 +0800

    add navigation bar and modify the animation

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # PersonalSite
 尝试做一个我的个人网站，可能会使用一部分vue（雾）
 
-#学习到的新东西
+# 学习到的新东西
 
-##html 部分
+## html 部分
 
 [利用@media screen实现网页布局的自适应](https://blog.csdn.net/inuyasha1121/article/details/50777116)
 
+顶部导航栏(head navigation bar)使用\<ul>标签实现而不是使用\<div>标签(是因为\<div>的样式设计过于繁琐？)
 
-##css 部分
+## css 部分
 
-##javascript 部分
+设置顶部导航栏(head navigation bar)固定在页面头
+
+
+```
+.head-bar {
+    position:fixed; 
+    top:0;
+}
+```
+
+当设置为`display:block;`之后，`animation`才会被触发。
+
+设置`animation-fill-mode:forwards`之后能保留动画效果。
+
+## javascript 部分
--- script.js
+++ script.js
@@ -13,11 +13,20 @@ let main_page = new Vue({
     }
 })
 
+function loading_page_fadeOut_FOR_DEBUG_NOT_LOADED_() {
+    loading_page.isLoad = true;
+    loading_page.isActive = false;
+    main_page.isNotActive = false;
+}
+
 function loading_page_fadeOut() {
     loading_page.isLoad = true;
-    setTimeout("loading_page.isActive = false;main_page.isNotActive = false;", 3000);
+    main_page.isNotActive = false;
+    setTimeout("loading_page.isActive = false;", 3000);
 }
 
 window.onload = function() {
     this.setTimeout("loading_page_fadeOut()", 3000);
+    // loading_page_fadeOut();
+    // loading_page_fadeOut_FOR_DEBUG_NOT_LOADED_();
 }
